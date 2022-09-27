8ef69d465338d02c009bc562069bbf8196f189f2
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-09-27 18:29:52 +0300

    Massive refactoring

--- ChampionScripts/Akali/E.cs
+++ ChampionScripts/Akali/E.cs
@@ -1,92 +1,48 @@
-using System.Linq;
-using GameServerCore;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
+using System.Numerics;
 using GameServerCore.Enums;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
-using System.Numerics;
 using LeagueSandbox.GameServer.API;
-using System.Collections.Generic;
 using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
 
 namespace Spells
 {
-    public class AkaliShadowSwipe : ISpellScript
+    public class AkaliMota : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
         {
+            MissileParameters = new MissileParameters
+            {
+                Type = MissileType.Target
+            },
             TriggersSpellCasts = true
+
             // TODO
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
+        public void OnActivate(ObjAIBase owner, Spell spell)
         {
             ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
         }
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-
-        }
-
-        public void OnSpellCast(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostCast(ISpell spell)
-        {
-            var sector = spell.CreateSpellSector(new SectorParameters
-            {
-                Length = 300f,
-                SingleTick = true,
-                Type = SectorType.Area
-            });
-        }
-        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
+       
+        public void TargetExecute(Spell spell, AttackableUnit target, SpellMissile missile, SpellSector sector)
         {
             var owner = spell.CastInfo.Owner;
-            if (!(target is IBaseTurret || target is ILaneTurret || target.Team == owner.Team || target == owner))
-            {
-                var AP = spell.CastInfo.Owner.Stats.AbilityPower.Total * 0.3f;
-                var AD = spell.CastInfo.Owner.Stats.AttackDamage.Total * 0.6f;
-                var damage = 40 + spell.CastInfo.SpellLevel * 30 + AP + AD;
-                var MarkAPratio = spell.CastInfo.Owner.Stats.AbilityPower.Total * 0.5f;
-                var MarkDamage = 45 + 25 * (owner.GetSpell("AkaliMota").CastInfo.SpellLevel - 1) + MarkAPratio;
-
-                if (target.HasBuff("AkaliMota"))
-                {
-                    target.TakeDamage(owner, MarkDamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_PROC, false);
-                    AddParticleTarget(owner, target, "akali_mark_impact_tar.troy", target, 1f);
-                    RemoveBuff(target, "AkaliMota");
-                }
-
-                target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
-                AddParticleTarget(owner, target, "akali_shadowSwipe_tar.troy", target, 1f);
-            }
+            var AP = owner.Stats.AbilityPower.Total * 0.4f;
+            var damage = 15f + spell.CastInfo.SpellLevel * 20f + AP;
+            
+            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
+            AddBuff("AkaliMota", 6f, 1, spell, target, owner);
+            AddBuff("AkaliMotaImpact", 6f, 1, spell, owner, owner);
+            missile.SetToRemove();
         }
 
-
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
+      
         }
     }
-}
