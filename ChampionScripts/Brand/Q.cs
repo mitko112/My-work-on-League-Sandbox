b33019814587e86884037afdf26fe0d7089f9f4f
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-09-27 20:23:50 +0300

    Massive refactoring. Fixed Q not working.

--- ChampionScripts/Brand/Q.cs
+++ ChampionScripts/Brand/Q.cs
@@ -1,51 +1,46 @@
-using System.Collections.Generic;
+
 using System.Numerics;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
 using GameServerCore.Enums;
-using LeagueSandbox.GameServer.API;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
 using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
 
 namespace Spells
 {
     public class BrandBlaze : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
             IsDamagingSpell = true
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-        }
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-        }
-
-        public void OnSpellCast(ISpell spell)
+        public void OnSpellCast(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
-            
+
         }
 
-        public void OnSpellPostCast(ISpell spell)
+        public void OnSpellPostCast(Spell spell)
         {
-            var owner = spell.CastInfo.Owner as IChampion;
+            var owner = spell.CastInfo.Owner as Champion;
             var ownerSkinID = owner.SkinID;
-            var targetPos = GetPointFromUnit(owner, 1100.0f);
-
+            var targetPos = new Vector2(spell.CastInfo.TargetPosition.X, spell.CastInfo.TargetPosition.Z);
+            var distance = Vector2.Distance(owner.Position, targetPos);
             FaceDirection(targetPos, owner);
-            
+
+            if (distance >= 1100.0)
+            {
+                targetPos = GetPointFromUnit(owner, 1100.0f);
+            }
+
             if (ownerSkinID == 5)
             {
                 SpellCast(owner, 3, SpellSlotType.ExtraSlots, targetPos, targetPos, false, Vector2.Zero);
@@ -55,28 +50,12 @@ namespace Spells
                 SpellCast(owner, 0, SpellSlotType.ExtraSlots, targetPos, targetPos, false, Vector2.Zero);
             }
         }
-
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
-        }
     }
 
     public class BrandBlazeMissile : ISpellScript
     {
-        IObjAiBase Owner;
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        ObjAIBase Owner;
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             MissileParameters = new MissileParameters
             {
@@ -85,38 +64,31 @@ namespace Spells
             IsDamagingSpell = true
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
+        public void OnActivate(ObjAIBase owner, Spell spell)
 
-           
+
         {
             ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
         }
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
 
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-            
-        }
 
-        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
+        public void TargetExecute(Spell spell, AttackableUnit target, SpellMissile missile, SpellSector sector)
         {
             var owner = spell.CastInfo.Owner;
             var APratio = owner.Stats.AbilityPower.Total * 0.65f;
-            var damage = 80 *(spell.CastInfo.SpellLevel - 0)  + APratio;
-             
+            var damage = 80 * (spell.CastInfo.SpellLevel - 0) + APratio;
 
-                if (target.HasBuff("BrandPassive"))
+
+            if (target.HasBuff("BrandPassive"))
             {
                 AddBuff("Stun", 2f, 1, spell, target, owner);
-                
+
             }
             target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
             AddParticleTarget(owner, target, "BrandBlaze_mis.troy", target, 1f, 1f);
             AddBuff("BrandPassive", 4f, 1, spell, target, owner, false);
-            
+
 
 
 
@@ -124,28 +96,9 @@ namespace Spells
 
         }
 
-public void OnSpellCast(ISpell spell)
-{
-}
 
-public void OnSpellPostCast(ISpell spell)
-{
-}
-
-public void OnSpellChannel(ISpell spell)
-{
-}
-
-public void OnSpellChannelCancel(ISpell spell)
-{
-}
-
-public void OnSpellPostChannel(ISpell spell)
-{
-}
-
-public void OnUpdate(float diff)
-{
-}
+        public void OnUpdate(float diff)
+        {
+        }
     }
 }
