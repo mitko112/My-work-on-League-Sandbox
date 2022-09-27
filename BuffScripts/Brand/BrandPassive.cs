64e9cbd078745f8be56c9d235a399d37dd060dd0
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-09-27 20:28:55 +0300

    Massive refactoring

--- BuffScripts/Brand/BrandPassive.cs
+++ BuffScripts/Brand/BrandPassive.cs
@@ -1,17 +1,21 @@
-using GameServerCore.Domain.GameObjects;
 using GameServerCore.Enums;
 using LeagueSandbox.GameServer.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell;
+
 using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.Stats;
+
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
 
 namespace Buffs
 
 {
-    internal class BrandPassive: IBuffGameScript
+    internal class CharScriptBrand: IBuffGameScript
     {
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.INTERNAL,
             BuffAddType = BuffAddType.REPLACE_EXISTING,
@@ -20,14 +24,14 @@ namespace Buffs
 
         
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        IObjAiBase owner;
-        IAttackableUnit Unit;
-        IParticle p;
+        ObjAIBase owner;
+        AttackableUnit Unit;
+        Particle p;
         float damage;
         float timeSinceLastTick = 500f;
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             owner = ownerSpell.CastInfo.Owner;
             Unit = unit;
@@ -37,7 +41,7 @@ namespace Buffs
             
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             RemoveParticle(p);
             
