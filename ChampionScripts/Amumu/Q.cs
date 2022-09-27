94477b101bb61191d443828d55a80150c7ab99c1
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-09-27 18:47:43 +0300

    Massive refactoring

--- ChampionScripts/Amumu/Q.cs
+++ ChampionScripts/Amumu/Q.cs
@@ -1,46 +1,33 @@
-using System.Collections.Generic;
 using System.Numerics;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
+
 using GameServerCore.Enums;
-using LeagueSandbox.GameServer.API;
+
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+
 
 namespace Spells
 {
     public class BandageToss : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
             IsDamagingSpell = true
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-        }
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
 
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-        }
 
-        public void OnSpellCast(ISpell spell)
-        {
-            
 
-        }
 
-        public void OnSpellPostCast(ISpell spell)
+
+        public void OnSpellPostCast(Spell spell)
         {
-            var owner = spell.CastInfo.Owner as IChampion;
+            var owner = spell.CastInfo.Owner as Champion;
             var ownerSkinID = owner.SkinID;
             var targetPos = GetPointFromUnit(owner, 1100.0f);
 
@@ -54,22 +41,6 @@ namespace Spells
                 SpellCast(owner, 0, SpellSlotType.ExtraSlots, targetPos, targetPos, false, Vector2.Zero);
             }
 
-        }
-
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
         }
     }
 }
