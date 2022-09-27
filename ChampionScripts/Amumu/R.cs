a52e5a461f000d64edbfbfdfa142101b0a6aa541
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-09-27 18:49:15 +0300

    Update R.cs

--- ChampionScripts/Amumu/R.cs
+++ ChampionScripts/Amumu/R.cs
@@ -1,49 +1,38 @@
-using System.Linq;
-using GameServerCore;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
 using GameServerCore.Enums;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
 using LeagueSandbox.GameServer.API;
-using System.Collections.Generic;
 using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
+using System;
 
 namespace Spells
 {
     public class CurseoftheSadMummy : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
         {
             TriggersSpellCasts = true
             // TODO
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
+        public void OnActivate(ObjAIBase owner, Spell spell)
         {
             ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
         }
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
+       
 
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        public void OnSpellPostCast(Spell spell)
         {
-        }
-
-        public void OnSpellCast(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostCast(ISpell spell)
-        {
-			var owner = spell.CastInfo.Owner;
-			var ownerSkinID = owner.SkinID;	
-            AddParticleTarget(owner, null, "CurseBandages_cas1.troy", owner);                                
+            var owner = spell.CastInfo.Owner;
+            var ownerSkinID = owner.SkinID;
+            AddParticleTarget(owner, null, "CurseBandages_cas1.troy", owner);
             spell.CreateSpellSector(new SectorParameters
             {
                 Length = 600f,
@@ -51,33 +40,18 @@ namespace Spells
                 Type = SectorType.Area
             });
         }
-        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
+        public void TargetExecute(Spell spell, AttackableUnit target, SpellMissile missile, SpellSector sector)
         {
             var owner = spell.CastInfo.Owner;
             if (owner != target)
             {
                 var AP = spell.CastInfo.Owner.Stats.AbilityPower.Total * 0.8f;
-                var damage = 150 + (spell.CastInfo.SpellLevel-1) * 100 + AP;
+                var damage = 150 + (spell.CastInfo.SpellLevel - 1) * 100 + AP;
                 target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
-				AddBuff("CurseoftheSadMummyCastEffects", 2f, 1, spell, target, owner);
+                AddBuff("CurseoftheSadMummyCastEffects", 2f, 1, spell, target, owner);
                 AddParticleTarget(owner, null, "CurseBandages.troy", target);
 
             }
-        }
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
         }
     }
 }
