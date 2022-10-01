46e1a901462e3391a2c17b30b13b443596e26283
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-01 12:14:22 +0300

    Massive refactoring

--- BuffScripts/Fizz/FizzSeastoneTrident.cs
+++ BuffScripts/Fizz/FizzSeastoneTrident.cs
@@ -1,49 +1,48 @@
-﻿using GameServerCore.Domain.GameObjects;
 using GameServerCore.Enums;
-using LeagueSandbox.GameServer.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell;
 using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.Stats;
-using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
 
 namespace Buffs
 {
     internal class FizzSeastoneTrident : IBuffGameScript
     {
-        
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.COMBAT_ENCHANCER,
             BuffAddType = BuffAddType.STACKS_AND_RENEWS,
             MaxStacks = 1
         };
 
-        
-
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
         private float timeSinceLastTick;
-        private IAttackableUnit Unit;
-        private float damage;
-        private IObjAiBase Owner;
-        private ISpell spell;
+        private AttackableUnit Unit;
+        private float TickingDamage;
+        private ObjAIBase Owner;
+        private Spell spell;
         private bool limiter = false;
 
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             var owner = ownerSpell.CastInfo.Owner;
             var missinghealth = (unit.Stats.HealthPoints.Total - unit.Stats.CurrentHealth);
-            
+            var PercentDamage = (3.5f + (ownerSpell.CastInfo.SpellLevel * 0.5f));
+            var AfterDamage = (missinghealth * 0.01f) * PercentDamage;
             var AP = ownerSpell.CastInfo.Owner.Stats.AbilityPower.Total * 0.45f;
-            
-             damage = AP + 30 + missinghealth*0.035f;
+            TickingDamage = (30f + AP + AfterDamage) * 0.33f;
+            var damage = AP + AfterDamage;
             Unit = unit;
             Owner = owner;
             spell = ownerSpell;
             limiter = true;
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
         }
 
@@ -53,7 +52,7 @@ namespace Buffs
 
             if (timeSinceLastTick >= 1000.0f)
             {
-                Unit.TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_PERIODIC, false);
+                Unit.TakeDamage(Owner, TickingDamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLPERSIST, false);
                 timeSinceLastTick = 0;
             }
         }
