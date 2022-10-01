7ef9535db6cfa7742de106f5fc0894e190011f41
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-01 11:38:32 +0300

    Massive refactoring

--- BuffScripts/Cassiopeia/CassiopeiaNoxiousBlastHaste.cs
+++ BuffScripts/Cassiopeia/CassiopeiaNoxiousBlastHaste.cs
@@ -1,32 +1,38 @@
-﻿using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Enums;
+using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
 using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
 
 namespace Buffs
 {
     internal class CassiopeiaNoxiousBlastHaste : IBuffGameScript
     {
-        public BuffType BuffType => BuffType.HASTE;
-        public BuffAddType BuffAddType => BuffAddType.STACKS_AND_OVERLAPS;
-        public int MaxStacks => 5;
-        public bool IsHidden => false;
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.HASTE,
+            BuffAddType = BuffAddType.STACKS_AND_OVERLAPS,
+            MaxStacks = 5
+        };
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             StatsModifier.MoveSpeed.PercentBonus += 20 / 100.0f;
             unit.AddStatModifier(StatsModifier);
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
-        }
+    
     }
 }
