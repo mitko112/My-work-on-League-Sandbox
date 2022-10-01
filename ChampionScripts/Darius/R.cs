9f4a9eff94f9383c74fccb076474401d61c79df9
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-01 11:59:35 +0300

    Massive refactoring

--- ChampionScripts/Darius/R.cs
+++ ChampionScripts/Darius/R.cs
@@ -1,41 +1,30 @@
-using LeagueSandbox.GameServer.API;
-using GameServerCore.Domain.GameObjects;
+sing GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Scripting.CSharp;
-using GameServerCore.Enums;
 namespace Spells
 {
     public class DariusExecute : ISpellScript
     {
-        IAttackableUnit Target;
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        AttackableUnit Target;
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
             IsDamagingSpell = true
             // TODO
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
             Target = target;
         }
 
-        public void OnSpellCast(ISpell spell)
-        {
-        }
 
-        public void OnSpellPostCast(ISpell spell)
+        public void OnSpellPostCast(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
             var ad = owner.Stats.AttackDamage.FlatBonus*1.5f;
@@ -49,17 +38,6 @@ namespace Spells
             AddParticleTarget(owner, Target, "darius_Base_R_tar.troy", Target, 1f, 1f);
         }
 
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
 
         public void OnUpdate(float diff)
         {
