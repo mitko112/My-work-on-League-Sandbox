91934e6b8e00a2152aa2cd0b0cce84368908c8fb
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-01 12:04:06 +0300

    Massive refactoring. Reworked scripting system.

--- ChampionScripts/Fizz/W.cs
+++ ChampionScripts/Fizz/W.cs
@@ -1,82 +1,77 @@
-﻿using System;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
-using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+﻿using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
+using GameServerCore.Enums;
 using GameServerCore.Scripting.CSharp;
 using LeagueSandbox.GameServer.API;
-using GameServerCore.Domain;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using GameServerLib.GameObjects.AttackableUnits;
 
 namespace Spells
 {
     public class FizzSeastonePassive : ISpellScript
     {
-        IAttackableUnit Target;
-        ISpell Spell;
-        IObjAiBase Owner;
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        
+        Spell Spell;
+        ObjAIBase Owner;
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true
             // TODO
         };
+        
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-            
-            Spell = spell;
-            Owner = owner;
-            ApiEventManager.OnLevelUpSpell.AddListener(this, spell, OnlevelUp, true);
-            AddBuff("FizzSeastoneTrident", 1f, 1, Spell, owner, owner, true);
-        }
+        public void OnActivate(ObjAIBase owner, Spell spell)
+        
+            {
+                
+                Spell = spell;
+                Owner = owner;
+                ApiEventManager.OnLevelUpSpell.AddListener(this, spell, AddFizzPassive, false);
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-            
-            
-        }
+                AddBuff("FizzMalison", 1f, 1, spell, Owner, Owner, true);
+            }
 
 
-
-        public void OnlevelUp(ISpell spell)
+        public void AddFizzPassive(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
-            ApiEventManager.OnHitUnit.AddListener(this, spell.CastInfo.Owner, OnHitUnit, false);
-        }
-        public void OnHitUnit(IDamageData damageData)
+            ApiEventManager.OnHitUnit.AddListener(this, owner, OnHitUnit, false);
 
+        }
+
+        public void OnDeactivate(ObjAIBase owner, Spell spell)
+        {
+        }
+
+        public void OnHitUnit(DamageData damageData)
 
         {
             var target = damageData.Target;
-            AddBuff("FizzSeastoneTrident", 3f, 1, Spell, target, Owner);
+            AddBuff("FizzMalison", 3f, 1, Spell, target, Owner);
         }
-
-
-        public void OnSpellCast(ISpell spell)
+        public void OnSpellCast(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
             AddBuff("FizzSeastoneActive", 5f, 1, spell, owner, owner);
 
         }
 
-        public void OnSpellPostCast(ISpell spell)
+        public void OnSpellPostCast(Spell spell)
         {
-            
+            var owner = spell.CastInfo.Owner;
         }
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell)
+        public void OnSpellChannel(Spell spell)
         {
         }
 
-        public void OnSpellPostChannel(ISpell spell)
+        public void OnSpellChannelCancel(Spell spell, ChannelingStopSource reason)
+        {
+        }
+
+        public void OnSpellPostChannel(Spell spell)
         {
         }
 
