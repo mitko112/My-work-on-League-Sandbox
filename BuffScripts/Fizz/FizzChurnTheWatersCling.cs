25dd577296bcba898f1cd772ed3d41ad3ed30991
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-01 12:10:17 +0300

    Massive refactoring

--- BuffScripts/Fizz/FizzChurnTheWatersCling.cs
+++ BuffScripts/Fizz/FizzChurnTheWatersCling.cs
@@ -1,35 +1,39 @@
-﻿using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
 using GameServerCore.Enums;
 using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.Stats;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+
 
 namespace Buffs
 {
     internal class FizzChurnTheWatersCling : IBuffGameScript
     {
-        
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+
+
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.COMBAT_ENCHANCER,
             BuffAddType = BuffAddType.RENEW_EXISTING,
             MaxStacks = 1
         };
-       
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        private IObjAiBase Owner;
-        private ISpell Spell;
-        private IAttackableUnit Target;
+        ObjAIBase Owner;
+        Spell Spell;
+        private AttackableUnit Target;
         private float ticks = 0;
         private float damage;
         private float true1 = 0;
 
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             Target = unit;
             Owner = ownerSpell.CastInfo.Owner;
@@ -43,11 +47,11 @@ namespace Buffs
             var units = GetUnitsInRange(Target.Position, 350f, true);
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
         }
 
-        public void OnPreAttack(ISpell spell)
+        public void OnPreAttack(Spell spell)
         {
         }
 
@@ -65,19 +69,19 @@ namespace Buffs
                     var units = GetUnitsInRange(Target.Position, 350f, true);
                     for (int i = units.Count - 1; i >= 0; i--)
                     {
-                        if (units[i].Team != Spell.CastInfo.Owner.Team && !(units[i] is IObjBuilding || units[i] is IBaseTurret) && units[i] is IObjAiBase ai)
+                        if (units[i].Team != Spell.CastInfo.Owner.Team && !(units[i] is ObjBuilding || units[i] is BaseTurret) && units[i] is ObjAIBase ai)
                         {
                             units[i].TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
                             var randPoint1 = new Vector2(units[i].Position.X + (40.0f), units[i].Position.Y + 40.0f);
 
-                            var xyz = units[i] as IObjAiBase;
+                            var xyz = units[i] as ObjAIBase;
                             xyz.SetTargetUnit(null);
 
-                            ForceMovement(units[i], "", randPoint1, 90.0f, 80.0f, 20.0f, 0.0f, ForceMovementType.FURTHEST_WITHIN_RANGE, ForceMovementOrdersType.CANCEL_ORDER, ForceMovementOrdersFacing.KEEP_CURRENT_FACING);
+                            ForceMovement(units[i], "", randPoint1, 90.0f, 80.0f, 20.0f, 0.0f, true, ForceMovementType.FURTHEST_WITHIN_RANGE, ForceMovementOrdersType.CANCEL_ORDER, ForceMovementOrdersFacing.KEEP_CURRENT_FACING);
                             units.RemoveAt(i);
                         }
                     }
-                    IMinion Fish = AddMinion(owner, "FizzShark", "FizzShark", Target.Position, owner.SkinID, false, false);
+                    Minion Fish = AddMinion(owner, "FizzShark", "FizzShark", Target.Position);
                     Fish.TakeDamage(Fish.Owner, 10000f, DamageType.DAMAGE_TYPE_TRUE, DamageSource.DAMAGE_SOURCE_INTERNALRAW, DamageResultType.RESULT_NORMAL);
                     Fish.PlayAnimation("SPELL4", 1.0f);
                     Fish.SetToRemove();
@@ -85,10 +89,10 @@ namespace Buffs
                     AddParticleTarget(Owner, Target, "Fizz_SharkSplash_Ground.troy", Target);
                     var randPoint = new Vector2(Target.Position.X + (40.0f), Target.Position.Y + 40.0f);
 
-                    var xy = Target as IObjAiBase;
+                    var xy = Target as ObjAIBase;
                     xy.SetTargetUnit(null);
 
-                    ForceMovement(Target, "", randPoint, 90.0f, 80.0f, 20.0f, 0.0f, ForceMovementType.FURTHEST_WITHIN_RANGE, ForceMovementOrdersType.CANCEL_ORDER, ForceMovementOrdersFacing.KEEP_CURRENT_FACING);
+                    ForceMovement(Target, "", randPoint, 90.0f, 80.0f, 20.0f, 0.0f, true, ForceMovementType.FURTHEST_WITHIN_RANGE, ForceMovementOrdersType.CANCEL_ORDER, ForceMovementOrdersFacing.KEEP_CURRENT_FACING);
                     true1 = 1;
                 }
             }
