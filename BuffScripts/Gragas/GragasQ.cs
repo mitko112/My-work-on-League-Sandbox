d28bd2455a537cad9c760dcdf4699aac54fa4301
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-04 21:32:36 +0300

    Massive refactoring

--- BuffScripts/Gragas/GragasQ.cs
+++ BuffScripts/Gragas/GragasQ.cs
@@ -1,56 +1,53 @@
-using System.Numerics;
-using GameServerCore;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
 using GameServerCore.Enums;
 using GameServerCore.Scripting.CSharp;
 using LeagueSandbox.GameServer.API;
-using LeagueSandbox.GameServer.GameObjects.Stats;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
 
 namespace Buffs
 {
     class GragasQ : IBuffGameScript
     {
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.COMBAT_ENCHANCER,
             BuffAddType = BuffAddType.REPLACE_EXISTING,
             MaxStacks = 1
         };
-        
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        ISpell ThisSpell;
-        IBuff ThisBuff;
-        IMinion Shadow;
-        IBuff ShadowBuff;
+        Spell ThisSpell;
+        Buff ThisBuff;
+        Minion Shadow;
+        Buff ShadowBuff;
         public bool QueueSwap;
         bool hascasted = false;
 
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             ThisSpell = ownerSpell;
             ThisBuff = buff;
 
-            ApiEventManager.OnSpellCast.AddListener(this, ownerSpell.CastInfo.Owner.GetSpell(0), TargetExecute);
+            ApiEventManager.OnSpellCast.AddListener(this, ownerSpell.CastInfo.Owner.GetSpell("GragasQ"), TargetExecute);
         }
-        public void TargetExecute(ISpell spell) 
-        { 
+        public void TargetExecute(Spell spell)
+        {
             hascasted = true;
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             if (hascasted == false) ownerSpell.CastInfo.Owner.SetSpell("GragasQ", 0, true);
             SealSpellSlot(ownerSpell.CastInfo.Owner, SpellSlotType.SpellSlots, 0, SpellbookType.SPELLBOOK_CHAMPION, false);
         }
 
-        
+
 
         public void OnUpdate(float diff)
         {
