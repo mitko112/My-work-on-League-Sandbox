d40b88eb201920b9e209efdc21d0d3628fd7db4e
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-04 21:42:54 +0300

    Massive refactoring

--- BuffScripts/Hecarim/EMSBUFF.cs
+++ BuffScripts/Hecarim/EMSBUFF.cs
@@ -14,63 +14,29 @@ using LeagueSandbox.GameServer.GameObjects.StatsNS;
 using LeagueSandbox.GameServer.GameObjects;
 
 
-
 namespace Buffs
 {
-    internal class HecarimRampAttack: IBuffGameScript
+    internal class HecarimRamp : IBuffGameScript
     {
         public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
-            BuffType = BuffType.DAMAGE,
+            BuffType = BuffType.HASTE,
             BuffAddType = BuffAddType.REPLACE_EXISTING,
-            MaxStacks = 1
+            MaxStacks = 5
         };
-
 
         public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        Buff thisBuff;
-        ObjAIBase Unit;
         
+
         public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
-            thisBuff = buff;
-            if (unit is ObjAIBase ai)
-            {
-                Unit = ai;
-
-                ApiEventManager.OnHitUnit.AddListener(this, ai, TargetExecute, true);
-
-                ai.SkipNextAutoAttack();
-
-
-            }
+            var owner =  ownerSpell.CastInfo.Owner;
+            StatsModifier.MoveSpeed.PercentBonus += 0.25f; 
+            unit.AddStatModifier(StatsModifier);
             
         }
 
-        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
-        {
-            //ApiEventManager.OnHitUnit.RemoveListener(this);
-
-        }
-        public void TargetExecute(DamageData damageData)
-        {
-            if (!thisBuff.Elapsed() && thisBuff != null && Unit != null)
-            {
-                float ad = Unit.Stats.AttackDamage.FlatBonus;
-                
-                float damage =  80*(Unit.GetSpell("HecarimRamp").CastInfo.SpellLevel - 2) + ad;
-                var trueCoords = GetPointFromUnit(Unit, 450f);
-                var target = damageData.Target;
-                target.TakeDamage(Unit, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
-                ForceMovement(target, "RUN", trueCoords, 2200, 0, 0, 0);
-
-                thisBuff.DeactivateBuff();
-            }
-        }
-        public void OnUpdate(float diff)
-        {
-
-        }
+      
     }
 }
