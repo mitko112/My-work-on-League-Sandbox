28c4a87c93fc172683ad3422dfc09ff4204114e4
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-05 13:01:42 +0300

    Massive refactoring

--- ChampionScripts/Irelia/R.cs
+++ ChampionScripts/Irelia/R.cs
@@ -1,21 +1,22 @@
-using System;
-using System.Numerics;
 using GameServerCore.Enums;
-using GameServerCore.Domain.GameObjects;
-using static LeagueSandbox.GameServer.API.ApiFunctionManager;
-using LeagueSandbox.GameServer.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
-using LeagueSandbox.GameServer.API;
-using System.Collections.Generic;
 using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
 namespace Spells
 {
     public class IreliaTranscendentBladesSpell : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata => new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
             MissileParameters = new MissileParameters
@@ -27,26 +28,20 @@ namespace Spells
             // TODO
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
+        public void OnActivate(ObjAIBase owner, Spell spell)
         {
             ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
         }
 
         
-         
-
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
 
          FaceDirection(end, owner);
 
         }
 
-        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
+        public void TargetExecute(Spell spell, AttackableUnit target, SpellMissile missile, SpellSector sector)
            {
             
                 var owner = spell.CastInfo.Owner;
@@ -56,12 +51,12 @@ namespace Spells
                 
 
                 target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-                AddParticleTarget(owner, target, "irelia_ult_tar.troy", target, lifetime: 1f);
+                AddParticleTarget(owner, owner, "irelia_ult_tar.troy", owner, lifetime: 1f);
 
                var units = GetUnitsInRange(owner.Position, 1000f, true);
                for (int i = 0; i < units.Count; i++)
             {
-                if (!(units[i].Team == owner.Team || units[i] is IBaseTurret || units[i] is IObjBuilding || units[i] is IInhibitor))
+                if (!(units[i].Team == owner.Team || units[i] is BaseTurret || units[i] is ObjBuilding || units[i] is Inhibitor))
                 {
                     units[i].TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
                     
@@ -76,28 +71,6 @@ namespace Spells
 
 
   
-  public void OnSpellCast(ISpell spell)
-{
-}
-
-public void OnSpellPostCast(ISpell spell)
-{
-}
-
-public void OnSpellChannel(ISpell spell)
-{
-}
-
-public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-{
-}
-
-public void OnSpellPostChannel(ISpell spell)
-{
-}
-
-public void OnUpdate(float diff)
-{
-}
     }
 }
+
