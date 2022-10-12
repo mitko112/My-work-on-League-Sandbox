9f519858ce7cb148c7e056c1a08181fe76c3fe04
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-12 22:04:38 +0300

    Massive refactoring

--- BuffScripts/Jax/JaxR passive.cs
+++ BuffScripts/Jax/JaxR passive.cs
@@ -1,83 +1,77 @@
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Enums;
-using GameServerCore.Scripting.CSharp;
+using System.Numerics;
 using LeagueSandbox.GameServer.API;
-using LeagueSandbox.GameServer.GameObjects.Stats;
-using LeagueSandbox.GameServer.Scripting.CSharp;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+
 
 
 namespace Buffs
 {
     internal class JaxRelentlessAttack : IBuffGameScript
     {
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.DAMAGE,
-            BuffAddType = BuffAddType.REPLACE_EXISTING,
+            BuffAddType = BuffAddType.STACKS_AND_RENEWS,
             MaxStacks = 3
         };
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        IAttackableUnit Unit;
+        AttackableUnit Unit;
 
 
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
-            
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+
         {
             Unit = unit;
             switch (buff.StackCount)
             {
-                    case 1:
-                    
+                case 1:
+
                     break;
-                    case 2:
-                    
+                case 2:
+
                     break;
-                    case 3:
+                case 3:
                     AddParticleTarget(ownerSpell.CastInfo.Owner, null, "RelentlessAssault_tar.troy", unit, 1f);
                     TargetExecute(ownerSpell);
                     buff.DeactivateBuff();
                     break;
-           
-               
+
+
 
             }
         }
-        public void TargetExecute(ISpell spell)
+        public void TargetExecute(Spell spell)
 
 
         {
 
             var owner = spell.CastInfo.Owner;
             var AP = owner.Stats.AbilityPower.Total * 0.7f;
-            
-            float damage = 100 * owner.GetSpell(1).CastInfo.SpellLevel + AP;
 
-           
-
-                Unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
-            
+            float damage = 100 * owner.GetSpell("JaxRelentlessAssault").CastInfo.SpellLevel + AP;
 
 
 
-        }
+            Unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
 
 
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
-        {
-            
-
-            
 
         }
 
-        public void OnUpdate(float diff)
-        {
 
-        }
     }
 }
