6ecc5f52e123af9066b1857458d3ff4abe2303e8
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-13 22:10:23 +0300

    Massive refactoring

--- ChampionScripts/Nasus/BasicAttack.cs
+++ ChampionScripts/Nasus/BasicAttack.cs
@@ -1,110 +1,57 @@
-﻿using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
+﻿using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
-using LeagueSandbox.GameServer.API;
-using GameServerCore.Scripting.CSharp;
-using GameServerCore.Enums;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
 namespace Spells
 {
     public class NasusBasicAttack : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             // TODO
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-        }
 
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
             ApiEventManager.OnLaunchAttack.AddListener(this, owner, OnLaunchAttack, false);
         }
 
-        public void OnLaunchAttack(ISpell spell)
+        public void OnLaunchAttack(Spell spell)
         {
             spell.CastInfo.Owner.SetAutoAttackSpell("NasusBasicAttack2", false);
         }
 
-        public void OnSpellCast(ISpell spell)
-        {
-        }
 
-        public void OnSpellPostCast(ISpell spell)
+        public class NasusBasicAttack2 : ISpellScript
         {
-        }
+            public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+            {
+                // TODO
+            };
 
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
 
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
 
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
+            public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
+            {
+                ApiEventManager.OnLaunchAttack.AddListener(this, owner, OnLaunchAttack, false);
+            }
 
-        public void OnUpdate(float diff)
-        {
-        }
-    }
-
-    public class NasusBasicAttack2 : ISpellScript
-    {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
-        {
-            // TODO
-        };
-
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-            ApiEventManager.OnLaunchAttack.AddListener(this, owner, OnLaunchAttack, false);
-        }
-
-        public void OnLaunchAttack(ISpell spell)
-        {
-            spell.CastInfo.Owner.SetAutoAttackSpell("NasusBasicAttack", false);
-        }
-
-        public void OnSpellCast(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostCast(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
+            public void OnLaunchAttack(Spell spell)
+            {
+                spell.CastInfo.Owner.SetAutoAttackSpell("NasusBasicAttack", false);
+            }
         }
     }
 }
