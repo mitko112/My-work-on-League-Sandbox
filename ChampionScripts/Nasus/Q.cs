044f0f12b1ead79a6d192f020c957fe9900b510f
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-13 22:12:07 +0300

    Massive refactoring. Q doesn't stack.

--- ChampionScripts/Nasus/Q.cs
+++ ChampionScripts/Nasus/Q.cs
@@ -1,132 +1,90 @@
 using GameServerCore.Enums;
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
-using LeagueSandbox.GameServer.API;
-using System.Collections.Generic;
-using GameServerCore.Domain.GameObjects.Spell.Missile;
-using GameServerCore.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell.Sector;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
 namespace Spells
 {
     public class NasusQ : ISpellScript
     {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
             NotSingleTargetSpell = true
             // TODO
         };
 
-        public void OnActivate(IObjAiBase owner, ISpell spell)
+        public void OnActivate(ObjAIBase owner, Spell spell)
         {
             ApiEventManager.OnLevelUpSpell.AddListener(this, spell, OnLevelUp, true);
         }
-        public void OnLevelUp(ISpell spell)
+        public void OnLevelUp(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
         }
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
+        public void OnDeactivate(ObjAIBase owner, Spell spell)
         {
         }
 
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
+        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
             AddBuff("NasusQ", 6.0f, 1, spell, owner, owner);
         }
 
-        public void OnSpellCast(ISpell spell)
-        {
-        }
 
-        public void OnSpellPostCast(ISpell spell)
+        public class NasusQAttack : ISpellScript
         {
-        }
-
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
-
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
-        }
-    }
-    public class NasusQAttack : ISpellScript
-    {
-        IAttackableUnit Target;
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
-        {
-            TriggersSpellCasts = true,
-            NotSingleTargetSpell = true,
-            IsDamagingSpell = true,
-            // TODO
-        };
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-        public void TargetExecute(ISpell spell, IAttackableUnit target, ISpellMissile missile, ISpellSector sector)
-        {
-        }
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-            Target = target;
-        }
-        public void OnSpellCast(ISpell spell)
-        {
-            var Owner = spell.CastInfo.Owner;
-            if (Owner.HasBuff("NasusQStacks"))
+            AttackableUnit Target;
+            public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
             {
-                float StackDamage = Owner.GetBuffWithName("NasusQStacks").StackCount;
-                float ownerdamage = spell.CastInfo.Owner.Stats.AttackDamage.Total;
-                float damage = 15 + 25 * Owner.GetSpell(0).CastInfo.SpellLevel + StackDamage + ownerdamage;
-                Target.TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
-                AddParticleTarget(Owner, Target, "Nasus_Base_Q_Tar.troy", Target);
-                if (Target.IsDead)
-                {
-                    AddBuff("NasusQStacks", 2500000f, 3, spell, Owner, Owner);
-                }
+                TriggersSpellCasts = true,
+                NotSingleTargetSpell = true,
+                IsDamagingSpell = true,
+                // TODO
+            };
 
+            public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
+            {
+                Target = target;
             }
-            else
+            public void OnSpellCast(Spell spell)
             {
-                float ownerdamage = spell.CastInfo.Owner.Stats.AttackDamage.Total;
-                float damage = 15 + 25 * Owner.GetSpell(0).CastInfo.SpellLevel + ownerdamage;
-                Target.TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
-                AddParticleTarget(Owner, Target, "Nasus_Base_Q_Tar.troy", Target);
-                if (Target.IsDead)
+                var Owner = spell.CastInfo.Owner;
+                if (Owner.HasBuff("NasusQStacks"))
                 {
-                    AddBuff("NasusQStacks", 2500000f, 3, spell, Owner, Owner);
+                    float StackDamage = Owner.GetBuffWithName("NasusQStacks").StackCount;
+                    float ownerdamage = spell.CastInfo.Owner.Stats.AttackDamage.Total;
+                    float damage = 15 + 25 * Owner.GetSpell("NasusQ").CastInfo.SpellLevel + StackDamage + ownerdamage;
+                    Target.TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
+                    AddParticleTarget(Owner, Target, "Nasus_Base_Q_Tar.troy", Target);
+                    if (Target.IsDead)
+                    {
+                        AddBuff("NasusQStacks", 2500000f, 3, spell, Owner, Owner);
+                    }
+
+                }
+                else
+                {
+                    float ownerdamage = spell.CastInfo.Owner.Stats.AttackDamage.Total;
+                    float damage = 15 + 25 * Owner.GetSpell("NasusQ").CastInfo.SpellLevel + ownerdamage;
+                    Target.TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, false);
+                    AddParticleTarget(Owner, Target, "Nasus_Base_Q_Tar.troy", Target);
+                    if (Target.IsDead)
+                    {
+                        AddBuff("NasusQStacks", 2500000f, 3, spell, Owner, Owner);
+                    }
                 }
             }
-        }
-        public void OnSpellPostCast(ISpell spell)
-        {
-        }
-        public void OnSpellChannel(ISpell spell)
-        {
-        }
-        public void OnSpellChannelCancel(ISpell spell, ChannelingStopSource reason)
-        {
-        }
-        public void OnSpellPostChannel(ISpell spell)
-        {
-        }
-        public void OnUpdate(float diff)
-        {
         }
     }
 }
