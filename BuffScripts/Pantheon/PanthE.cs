e185ee910bf1547d48b50ffadb952a5e287e75ea
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-15 21:42:19 +0300

    Massive refactoring

--- BuffScripts/Pantheon/PanthE.cs
+++ BuffScripts/Pantheon/PanthE.cs
@@ -1,31 +1,39 @@
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Enums;
-using LeagueSandbox.GameServer.Scripting.CSharp;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.Stats;
+using System.Numerics;
+using LeagueSandbox.GameServer.API;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+
 
 namespace Buffs
 
 {
     internal class PantheonE : IBuffGameScript
     {
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.POISON,
             BuffAddType = BuffAddType.REPLACE_EXISTING,
             MaxStacks = 1
         };
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        IObjAiBase owner;
-        IAttackableUnit Unit;
-        IParticle p;
+        ObjAIBase owner;
+        AttackableUnit Unit;
+        Particle p;
         float damage;
         float timeSinceLastTick = 500f;
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             owner = ownerSpell.CastInfo.Owner;
             Unit = unit;
@@ -36,7 +44,7 @@ namespace Buffs
 
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnDeactivate(AttackableUnit unit,Buff buff, Spell ownerSpell)
         {
             RemoveParticle(p);
         }
