8448b0a9937873caa75ad73f55f8e70b92aed40a
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-10-15 21:36:35 +0300

    Massive refactoring

--- BuffScripts/Nasus/NasusQStacks.cs
+++ BuffScripts/Nasus/NasusQStacks.cs
@@ -1,47 +1,39 @@
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Enums;
-using GameServerCore.Scripting.CSharp;
+using System.Numerics;
 using LeagueSandbox.GameServer.API;
-using LeagueSandbox.GameServer.GameObjects.Stats;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
-using GameServerCore.Domain;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+
 
 namespace Buffs
 {
     internal class NasusQStacks : IBuffGameScript
     {
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.COUNTER,
             BuffAddType = BuffAddType.STACKS_AND_RENEWS,
             MaxStacks = 999999
         };
 
-        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
 
-        IBuff thisBuff;
-        IObjAiBase Unit;
-        IParticle p;
-        IParticle p2;
-        IObjAiBase Owner;
-        IAttackableUnit AtOwner;
+        Buff thisBuff;
+        ObjAIBase Unit;
+        Particle p;
+        Particle p2;
+        ObjAIBase Owner;
+        AttackableUnit AtOwner;
         public static float Qstacks = 0f;
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
-        {
-            //ApiEventManager.OnKillUnit.AddListener(this, AtOwner, NasusMoreStacks, true);
-        }
-
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
-        {
-            //ApiEventManager.OnHitUnit.RemoveListener(this);
-
-        }
-
-        public void OnUpdate(float diff)
-        {
-
+       
         }
     }
-}
