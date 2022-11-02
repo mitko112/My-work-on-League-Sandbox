4fa5c79a2142b1843ba60ae7112e34f39655ad76
mitko112 <92447420+mitko112@users.noreply.github.com>
2022-11-02 21:00:39 +0200

    Massive refactoring

--- BuffScripts/Renekton/Ebuff.cs
+++ BuffScripts/Renekton/Ebuff.cs
@@ -1,32 +1,42 @@
-using GameServerCore.Domain.GameObjects;
-using GameServerCore.Domain.GameObjects.Spell;
-using GameServerCore.Enums;
-using GameServerCore.Scripting.CSharp;
+using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+
 
 namespace Buffs
 {
     internal class RenektonSliceAndDice : IBuffGameScript
     {
-        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.INTERNAL,
             BuffAddType = BuffAddType.REPLACE_EXISTING,
             MaxStacks = 1
         };
 
-        public IStatsModifier StatsModifier { get; private set; }
+        public StatsModifier StatsModifier { get; private set; }
 
-        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
-            ((IObjAiBase)unit).SetSpell("RenektonDice", 2, true);
+            ((ObjAIBase)unit).SetSpell("RenektonDice", 2, true);
         }
 
-        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
-            if (((IObjAiBase)unit).Spells[2].SpellName == "RenektonDice")
+            if (((ObjAIBase)unit).Spells[2].SpellName == "RenektonDice")
             {
-                ((IObjAiBase)unit).SetSpell("RenektonSliceAndDice", 2, true);
+                ((ObjAIBase)unit).SetSpell("RenektonSliceAndDice", 2, true);
             }
         }
 
