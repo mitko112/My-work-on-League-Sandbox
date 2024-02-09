d4efb1b8b051f5a38e115e6577bf3c2470504dfa
mitko112 <92447420+mitko112@users.noreply.github.com>
2024-02-09 21:08:15 +0200

    Add files via upload

--- ChampionScripts/Vayne/Passive.cs
+++ ChampionScripts/Vayne/Passive.cs
@@ -1,25 +1,45 @@
 ﻿
-using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore.Enums;
 using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeaguePackets.Game;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using static LeaguePackets.Game.Common.CastInfo;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
-namespace Passives
+
+namespace CharScripts
 {
-    public class Focus : ICharScript
+
+    public class CharScriptVayne : ICharScript
+
     {
-
-        Spell originspell;
-        ObjAIBase ownermain;
-
+        
         public void OnActivate(ObjAIBase owner, Spell spell)
+
         {
-            originspell = spell;
-            ownermain = owner;
-            AddBuff("PassiveMSVayne", float.MaxValue, 1, spell, owner, spell.CastInfo.Owner);
+            AddBuff("PassiveMSVayne", 5f, 1, spell, owner, owner, true);
+
         }
 
-  
+
+        public void OnDeactivate(ObjAIBase owner, Spell spell)
+        {
+
+        }
+        public void OnUpdate(float diff)
+        {
+        }
     }
 }
-
