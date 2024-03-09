343e2180bc2b28f31b2da6d2bbfa85e6d4e51016
mitko112 <92447420+mitko112@users.noreply.github.com>
2024-03-09 16:51:20 +0200

    Add files via upload

---
+++ ChampionScripts/Gragas/Buffs/Passive.cs
@@ -0,0 +1,41 @@
+using GameServerCore.Enums;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.API;
+using GameServerLib.GameObjects.AttackableUnits;
+using System.Numerics;
+
+
+namespace Buffs
+{
+    internal class GragasPassive : IBuffGameScript
+    {
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.COMBAT_ENCHANCER,
+            BuffAddType = BuffAddType.REPLACE_EXISTING,
+            MaxStacks = 1
+        };
+
+
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            
+            float heal = unit.Stats.HealthPoints.Total * 0.05f;
+            StatsModifier.HealthRegeneration.PercentBonus = heal;
+            unit.Stats.CurrentHealth += heal;
+            unit.AddStatModifier(StatsModifier);
+            
+        }
+
+    
+    }
+}
