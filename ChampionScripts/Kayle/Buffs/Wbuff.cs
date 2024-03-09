e7ba8ba0475b28e70e54183f4325ea4e9003a7e4
mitko112 <92447420+mitko112@users.noreply.github.com>
2024-03-09 16:54:27 +0200

    Add files via upload

---
+++ ChampionScripts/Kayle/Buffs/Wbuff.cs
@@ -0,0 +1,50 @@
+using System.Numerics;
+using LeagueSandbox.GameServer.API;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+
+namespace Buffs
+{
+    internal class JudicatorDivineBlessing : IBuffGameScript
+    {
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.HASTE,
+            BuffAddType = BuffAddType.REPLACE_EXISTING,
+            MaxStacks = 1
+        };
+
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        Particle p;
+
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+
+            StatsModifier.MoveSpeed.PercentBonus = 0.18f;
+
+            unit.AddStatModifier(StatsModifier);
+            
+        }
+
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            RemoveParticle(p);
+        }
+
+        public void OnUpdate(float diff)
+        {
+
+        }
+    }
+}
