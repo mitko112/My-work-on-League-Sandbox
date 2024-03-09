5f5d0afa891440854850b6fecf70feea0056739b
mitko112 <92447420+mitko112@users.noreply.github.com>
2024-03-09 16:59:15 +0200

    Add files via upload

---
+++ ChampionScripts/Ryze/Buffs/RyzeWDebuff.cs
@@ -0,0 +1,47 @@
+﻿using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace Buffs
+{
+    internal class RyzeWDebuff : IBuffGameScript
+    {
+        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.COMBAT_ENCHANCER,
+            BuffAddType = BuffAddType.REPLACE_EXISTING,
+            MaxStacks = 1
+        };
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        private IParticle buff1;
+        private IParticle buff2;
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            var caster = ownerSpell.CastInfo.Owner;
+            buff1 = AddParticleTarget(caster, unit, "RunePrison_cas.troy", unit);
+            buff2 = AddParticleTarget(caster, unit, "RunePrison_tar.troy", unit);
+            unit.SetStatus(StatusFlags.CanMove, false);
+            unit.SetStatus(StatusFlags.Rooted, true);
+            unit.StopMovement();
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            RemoveParticle(buff1);
+            RemoveParticle(buff2);
+            unit.SetStatus(StatusFlags.CanMove, true);
+            unit.SetStatus(StatusFlags.Rooted, false);
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
